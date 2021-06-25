736bfe414693cb0cbe753853e40ae7736cfa434b
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-06-24 20:38:19 -0400

    .

--- src/Components/HomeMenu/index.js
+++ src/Components/HomeMenu/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+import { ReactComponent as DiscordSvg } from "../../Styles/svg/Discord-Logo-Black.svg";
 function HomeMenu() {
   return (
     <StyledMenu>
@@ -20,6 +20,11 @@ function HomeMenu() {
           <Link to="/setting">Settings</Link>
         </li>
       </ul>
+      <div className="discordSvg">
+        <a href="https://discord.gg/hfHjjwXM">
+          <DiscordSvg className="discord" target="_blank"/>
+        </a>
+      </div>
     </StyledMenu>
   );
 }
@@ -27,7 +32,6 @@ function HomeMenu() {
 const StyledMenu = styled.div`
   h1 {
     font-size: 80px;
-    
     color: white;
   }
   ul {
@@ -52,6 +56,26 @@ const StyledMenu = styled.div`
       cursor: pointer;
       :hover {
         color: gray;
+      }
+    }
+  }
+  .discordSvg {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    margin-bottom: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .discord {
+      height: 25px;
+      width: 25px;
+      fill: #23272a;
+      transition: 0.5s;
+      cursor: pointer;
+      :hover {
+        fill: black;
       }
     }
   }
